413a5cb61495ab885dce17feb65ed0783550fcde
leodinh <NguyenAnhTuan.Dinh@georgebrown.ca>
2020-02-08 00:52:41 -0500

    added wallet page, added fetching data for fund site

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext, useCallback } from "react";
 import "./App.css";
 import Layout from "./hoc/Layout/Layout";
 import Funds from "./container/Funds/Funds";
+import Wallet from "./container/Wallet/Wallet";
 import RequestForm from "./container/RequestForm/RequestForm";
 import getWeb3 from "./getWeb3";
 import { Route, Switch, Redirect } from "react-router-dom";
@@ -10,10 +11,6 @@ function App() {
   const [accounts, setAccounts] = useState("");
   const [web3, setWeb3] = useState("");
   const web3Context = useContext(Web3Context);
-  const [balance, setBalance] = useState("");
-  useEffect(() => {
-    getAccounts();
-  }, [getAccounts]);
   const getAccounts = useCallback(async () => {
     const web3 = await getWeb3();
     setWeb3(web3);
@@ -21,22 +18,19 @@ function App() {
     setAccounts(accounts);
     web3Context.setAccounts(accounts);
     web3Context.setWeb3(web3);
-    const balance = await web3.eth.getBalance(accounts[0]);
-    console.log(accounts[0]);
-    console.log(balance);
-    window.ethereum.on("accountsChanged", async accounts => {
-      accounts = await web3.eth.getAccounts();
-      setAccounts(accounts);
-    });
-  });
+  }, [web3Context]);
+  useEffect(() => {
+    getAccounts();
+  }, [getAccounts]);
   return (
     <Layout>
       <Switch>
-        <Route path="/fund" render={() => <Funds account={accounts[0]} />} />
+        <Route path="/fund" render={() => <Funds />} />
         <Route
           path="/borrow"
           render={() => <RequestForm account={accounts[0]} web3={web3} />}
         />
+        <Route path="/wallet" render={() => <Wallet />} />
         <Redirect from="/" to="/fund" />
       </Switch>
     </Layout>
